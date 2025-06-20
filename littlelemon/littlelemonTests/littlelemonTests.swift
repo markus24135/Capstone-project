bcc732df49f0f50116af668f187e52549c939fb7
Mark Mueller <markus24135@hotmail.com>
2025-06-20 17:04:39 -0500

    Initial commit: added littlelemon Xcode project

---
+++ littlelemon/littlelemonTests/littlelemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonTests.swift
+//  littlelemonTests
+//
+//  Created by Mark Mueller on 6/20/25.
+//
+
+import Testing
+@testable import littlelemon
+
+struct littlelemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
